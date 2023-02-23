025ad560d39e2b1cc958bc006c4211e2563c46e9
krum04 <krum04@gmail.com>
2023-02-22 22:16:32 -0600

    First Commit

---
+++ folderSync.py
@@ -0,0 +1,46 @@
+# folderSync.py
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+clientSecrets = 'client_secrets.json' # path to client_secrets.json file
+
+def folderSync():
+    try:
+        # Authenticate with Google Drive
+        gauth = GoogleAuth()
+        scope = ["https://www.googleapis.com/auth/drive"]
+        gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(clientSecrets, scope)
+        drive = GoogleDrive(gauth)
+
+        # Get a list of file names in the local directory and the Google Drive directory
+        local_file_names = os.listdir('images')
+        drive_file_names = [file['title'] for file in drive.ListFile().GetList() if file['mimeType'].split('/')[0] == 'image']
+
+        # Sort the lists of file names so they can be compared
+        local_file_names.sort()
+        drive_file_names.sort()
+
+        # If the lists of file names are not equal, delete any local files that are not in the Google Drive directory
+        # and download any Google Drive files that are not in the local directory
+        if local_file_names != drive_file_names:
+            print('different list')     
+            for file in local_file_names:
+                if file not in drive_file_names:
+                    os.remove(f'images/{file}')
+
+            for file in drive.ListFile().GetList():
+                if file['mimeType'].split('/')[0] == 'image':
+                    if file['title'] not in local_file_names:
+                        file.GetContentFile(f"images/{file['title']}")
+
+        # Create a list of file paths in the local directory and return it
+        list_return = [f'images/{file}' for file in os.listdir('images')]
+        return list_return
+    
+    # If there is an exception, return a list of file paths in the local directory
+    except Exception as e:
+        print(e)
+        list_return = [f'images/{file}' for file in os.listdir('images')]
+        return list_return
---
+++ main.py
@@ -0,0 +1,60 @@
+# main.py
+import tkinter
+from PIL import Image, ImageTk, ImageFilter
+from folderSync import folderSync 
+
+# create a Tkinter window
+root = tkinter.Tk()
+
+# get the screen dimensions
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+
+# set window properties
+root.overrideredirect(1) 
+root.geometry(f"{w}x{h}+0+0") 
+root.focus_set()  
+
+# create a canvas to draw on
+canvas = tkinter.Canvas(root, width=w, height=h, highlightthickness=0)
+canvas.pack()
+canvas.configure(background='black')  
+
+# set up an infinite loop to display images
+running = True
+
+# seconds to display each image
+delay = 10  
+
+while running:
+
+    # get a list of image files from the specified folder
+    images = folderSync()
+
+    # loop through the images and display each one
+    for image in images:
+
+        # attempt to open the image
+        try:
+            pilImage = Image.open(image)
+        except Exception as e:
+            print(e)
+            continue  # skip to the next image if there's an error
+
+        # resize the image to fit the screen while preserving aspect ratio
+        imgWidth, imgHeight = pilImage.size
+        ratio = min(w/imgWidth, h/imgHeight)
+        imgWidth = int(imgWidth*ratio)
+        imgHeight = int(imgHeight*ratio)
+
+        # create a blurred version of the image and paste the original onto it
+        bluredImage = pilImage.resize((2000, 2000), Image.ANTIALIAS).filter(ImageFilter.GaussianBlur(radius=30))
+        pilImage = pilImage.resize((imgWidth, imgHeight), Image.ANTIALIAS)
+        bluredImage.paste(pilImage, (int(1000-(imgWidth/2)), int(1000-(imgHeight/2))))
+
+        # convert the image to a Tkinter-compatible format and display it
+        imageTk = ImageTk.PhotoImage(bluredImage)
+        imagesprite = canvas.create_image(w/2, h/2, image=imageTk)
+
+        # update the window and wait for 10 seconds before displaying the next image
+        root.update()
+        root.after(delay*1000)
